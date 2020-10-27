ef75e523a043fa2db6ce228a24ebc6d6bd6e6929
Pedro Chaves <pedrohbchaves@gmail.com>
2020-10-27 11:15:35 -0300

    server js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+app.use(cors());
+
+//CORS Middleware
+
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
+    next();
+    });
+    
+
+app.use(express.static(__dirname + '/dist/jatoba'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/jatoba/index.html'));});
+app.listen(process.env.PORT || 8080);
