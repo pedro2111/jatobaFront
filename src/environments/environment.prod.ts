08d3ebee4be7401ddb04d92139b8eebf8d07e79a
Pedro Chaves <pedrohbchaves@gmail.com>
2020-10-27 11:49:10 -0300

    troca de url site para o do heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   ambiente:"prd",
   URL_API:"https://jatoba-api.herokuapp.com",
-  URL_SITE:'http://localhost:4200'
+  URL_SITE:'https://moveisrusticosjatoba.herokuapp.com'
 };
